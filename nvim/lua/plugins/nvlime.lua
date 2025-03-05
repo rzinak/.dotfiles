30bb6584ddcb998702c3742c66ca255b145f365b
Renan Araújo <awkrenan@gmail.com>
2025-03-04 22:33:58 -0300

    updated dotfiles

---
+++ nvim/lua/plugins/nvlime.lua
@@ -0,0 +1,16 @@
+return {
+  'monkoose/nvlime',
+  -- version = '*',
+  dependencies = {
+    'monkoose/parsley',
+  },
+  -- opts = {
+  --   filesystem = {
+  --     window = {
+  --       mappings = {
+  --         ['\\'] = 'close_window',
+  --       },
+  --     },
+  --   },
+  -- },
+}
