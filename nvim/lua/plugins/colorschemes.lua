9cf9b9128c8952e01c4d3b6a6d522120d137de0f
Renan Araújo <awkrenan@gmail.com>
2025-05-31 16:51:17 -0300

    updated dotfiles

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,23 +1,19 @@
 return {
-  { 'rebelot/kanagawa.nvim' },
-  { 'HoNamDuong/hybrid.nvim' },
-  {
-    'rzinak/gruber-darker.nvim',
-    opts = {
-      bold = false,
-      invert = {
-        signs = false,
-        tabline = false,
-        visual = false,
-      },
-      italic = {
-        strings = false,
-        comments = true,
-        operators = false,
-        folds = true,
-      },
-      undercurl = true,
-      underline = true,
+  'rzinak/gruber-darker.nvim',
+  opts = {
+    bold = false,
+    invert = {
+      signs = false,
+      tabline = false,
+      visual = false,
     },
+    italic = {
+      strings = false,
+      comments = true,
+      operators = false,
+      folds = true,
+    },
+    undercurl = true,
+    underline = true,
   },
 }
